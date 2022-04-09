e901a8a87b456e703d4c5c8ac4317077edb62d37
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-09 12:14:31 +0200

    Added product property as eg.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Product} from "./model/product";
 
 @Component({
   selector: 'ac-root',
@@ -81,5 +82,24 @@ import {Component} from '@angular/core';
   `]
 })
 export class AppComponent {
+  product: Product = {
+    "id": 1,
+    "label": "myMac",
+    "description": "Inspired by the best of Apple. Transformed by the M1 chip. Stands out in any space. Fits perfectly into your life.",
+    "image": "https://res.cloudinary.com/my-notes-demo/image/upload/v1627760417/academy/imac.png",
+    "memory": 16000,
+    "storage": 2000000,
+    "display": 24,
+    "price": 1500,
+    "colors": [
+      "mediumpurple", "red", "yellow", "aquamarine", "deepskyblue"
+    ]
+  }
 
+  /**
+   * The constructor method.
+   */
+  constructor() {
+    console.log(this.product)
+  }
 }
