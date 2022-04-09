4f098a3179be2b50d3c594e129ceb9082873fb6d
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-09 14:52:56 +0200

    Looping products.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,76 +6,81 @@ import {Product} from "./model/product";
   template: `
     <!--container-->
     <div class="container mx-auto p-4 row">
-      <div class="col-4">
+      <div class="row row-cols-1 row-cols-lg-2  row-cols-xl-3 mt-5">
 
-        <!--card-->
-        <div class="card h-100 round-border shadow-lg">
-          <!--image-->
-          <div class="position-relative">
-            <img
-              class="card-img-top p-3 round-border "
-              [src]="product.image" [alt]="product.label"
-              role="button"
-            >
-            <div class="position-absolute top-50 start-50 w-75 translate-middle pe-none">
-              <div class="row  row-cols-3 g-2 rounded p-2 text-white" style="background: rgba(0,0,0,0.5)">
-                <div class="col text-center">
-                  <i class="fas fa-memory"></i>
-                  {{product.memory / 1000 }}
-                </div>
+        <!--cards loop-->
+        <div class="col" *ngFor="let product of products">
 
-                <div class="col text-center">
-                  <i class="fas fa-sim-card"></i>
-                  {{product.storage / 1000}}
+          <!--card-->
+          <div class="card h-100 round-border shadow-lg">
+            <!--image-->
+            <div class="position-relative">
+              <img
+                class="card-img-top p-3 round-border "
+                [src]="product.image" [alt]="product.label"
+                role="button"
+              >
+              <div class="position-absolute top-50 start-50 w-75 translate-middle pe-none">
+                <div class="row  row-cols-3 g-2 rounded p-2 text-white" style="background: rgba(0,0,0,0.5)">
+                  <div class="col text-center">
+                    <i class="fas fa-memory"></i>
+                    {{product.memory / 1000 }}
+                  </div>
+
+                  <div class="col text-center">
+                    <i class="fas fa-sim-card"></i>
+                    {{product.storage / 1000}}
+                  </div>
+                  <div class="col text-center">
+                    <i class="fas fa-mobile-alt"></i>
+                    {{product.display}}''
+                  </div>
                 </div>
-                <div class="col text-center">
-                  <i class="fas fa-mobile-alt"></i>
-                  {{product.display}}''
-                </div>
+              </div>
+            </div>
+
+            <!--title and description-->
+            <div class="card-body text-center">
+              <h5 class="card-title">
+                {{product.label}}
+              </h5>
+              <p class="card-text text-secondary">{{product.description}}</p>
+            </div>
+
+            <!--colors-->
+            <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center">
+              <div
+                class="mx-1 mb-3 round-border shadow-sm"
+                style="width: 30px; height: 30px; border: 1px solid #fff"
+                role="button"
+                *ngFor="let color of product.colors"
+                (click)="selectedColor = color"
+                [style.backgroundColor]="color"
+                [style.border-width.px]="selectedColor === color ? 5 : 1"
+                [style.border-color]="selectedColor === color ? 'orange' : '#ccc'"
+              ></div>
+            </div>
+
+            <!--footer-->
+            <div class="card-footer d-flex justify-content-between align-items-center">
+              <div class="h5 text-secondary">{{product.price | currency: '€'}} + vat</div>
+
+              <div>
+                <span>select color</span>
+
+                <button
+                  class="btn icon-circle-sm px-1 ms-2 shadow-lg"
+                  style="color: #bbb"
+                  [style.background-color]="selectedColor  || '#ccc'"
+                  [disabled]="!selectedColor"
+                  (click)="addToCartHandler({ product: product, color: selectedColor })"
+                >
+                  <i class="fas fa-cart-plus text-white"></i>
+                </button>
               </div>
             </div>
           </div>
 
-          <!--title and description-->
-          <div class="card-body text-center">
-            <h5 class="card-title">
-              {{product.label}}
-            </h5>
-            <p class="card-text text-secondary">{{product.description}}</p>
-          </div>
-
-          <!--colors-->
-          <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center">
-            <div
-              class="mx-1 mb-3 round-border shadow-sm"
-              style="width: 30px; height: 30px; border: 1px solid #fff"
-              role="button"
-              *ngFor="let color of product.colors"
-              (click)="selectedColor = color"
-              [style.backgroundColor]="color"
-              [style.border-width.px]="selectedColor === color ? 5 : 1"
-              [style.border-color]="selectedColor === color ? 'orange' : '#ccc'"
-            ></div>
-          </div>
-
-          <!--footer-->
-          <div class="card-footer d-flex justify-content-between align-items-center">
-            <div class="h5 text-secondary">{{product.price | currency: '€'}} + vat</div>
-
-            <div>
-              <span>select color</span>
-
-              <button
-                class="btn icon-circle-sm px-1 ms-2 shadow-lg"
-                style="color: #bbb"
-                [style.background-color]="selectedColor  || '#ccc'"
-                [disabled]="!selectedColor"
-                (click)="addToCartHandler({ product: product, color: selectedColor })"
-              >
-                <i class="fas fa-cart-plus text-white"></i>
-              </button>
-            </div>
-          </div>
         </div>
 
       </div>
@@ -90,19 +95,50 @@ import {Product} from "./model/product";
   `]
 })
 export class AppComponent {
-  product: Product = {
-    "id": 1,
-    "label": "myMac",
-    "description": "Inspired by the best of Apple. Transformed by the M1 chip. Stands out in any space. Fits perfectly into your life.",
-    "image": "https://res.cloudinary.com/my-notes-demo/image/upload/v1627760417/academy/imac.png",
-    "memory": 16000,
-    "storage": 2000000,
-    "display": 24,
-    "price": 1500,
-    "colors": [
-      "mediumpurple", "red", "yellow", "aquamarine", "deepskyblue"
-    ]
-  }
+
+  products: Product[] = [
+    {
+      "id": 1,
+      "label": "myMac",
+      "description": "Inspired by the best of Apple. Transformed by the M1 chip. Stands out in any space. Fits perfectly into your life.",
+      "image": "https://res.cloudinary.com/my-notes-demo/image/upload/v1627760417/academy/imac.png",
+      "memory": 16000,
+      "storage": 2000000,
+      "display": 24,
+      "price": 1500,
+      "colors": [
+        "mediumpurple", "red", "yellow", "aquamarine", "deepskyblue"
+      ]
+    },
+    {
+      "id": 3,
+      "label": "myPad Pro",
+      "description": "Now with breakthrough M1 performance, a breathtaking XDR display, and blazing‑fast 5G wireless.",
+      "image": "https://res.cloudinary.com/my-notes-demo/image/upload/v1627760421/academy/ipad.png",
+      "memory": 4000,
+      "storage": 64000,
+      "display": 6.1,
+      "price": 900,
+      "colors": [
+        "dimgray", "darkgray"
+      ]
+
+    },
+    {
+      "id": 4,
+      "label": "myPhone 13",
+      "description": "5G speed. A14 Bionic, the fastest chip in a smartphone.\nAn edge-to-edge OLED display. Ceramic Shield with four times better drop performance. And Night mode on every camera. iPhone 12 has it all — in two perfect sizes.",
+      "image": "https://res.cloudinary.com/my-notes-demo/image/upload/v1627760465/academy/iphone12.jpg",
+      "memory": 16000,
+      "storage": 256000,
+      "display": 12.9,
+      "price": 850,
+      "colors": [
+        "black", "red", "lightgreen", "midnightblue", "mediumpurple"
+      ]
+
+    }
+  ];
 
   selectedColor: string | null = null;
 
@@ -110,7 +146,7 @@ export class AppComponent {
    * The constructor method.
    */
   constructor() {
-    console.log(this.product)
+    console.log(this.products)
   }
 
   /**
