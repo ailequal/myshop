dfe1401449a12112a265cc15b64b1c7eb82cf77a
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-13 11:50:01 +0200

    Added backoffice to the fake menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,19 @@ import {Component} from '@angular/core';
     <div class="navigation" style="display: flex; justify-content: center; align-content: center">
       <button (click)="page = 'shop'">SHOP</button>
       <button (click)="page = 'cart'">CART</button>
+      <button (click)="page = 'backoffice'">BACKOFFICE</button>
     </div>
 
     <!--if statement-->
     <!--    <ac-shop *ngIf="page === 'shop'"></ac-shop>-->
     <!--    <ac-cart *ngIf="page === 'cart'"></ac-cart>-->
+    <!--    <ac-cart *ngIf="page === 'backoffice'"></ac-cart>-->
 
     <!--switch case statement-->
     <ng-container [ngSwitch]="page">
       <ac-shop *ngSwitchCase="'shop'"></ac-shop>
       <ac-cart *ngSwitchCase="'cart'"></ac-cart>
+      <ac-backoffice *ngSwitchCase="'backoffice'"></ac-backoffice>
       <h2 *ngSwitchDefault>I will never be printed.</h2>
     </ng-container>
   `,
@@ -24,7 +27,7 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  page: 'cart' | 'shop' = 'cart';
+  page: 'cart' | 'shop' | 'backoffice' = 'backoffice';
 
   /**
    * The constructor method.
