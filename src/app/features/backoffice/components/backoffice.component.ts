c45c6058feb1442d6bf1edac4e10705017bc296f
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-14 10:02:00 +0200

    Basic navigation for backoffice.

--- src/app/features/backoffice/components/backoffice.component.ts
+++ src/app/features/backoffice/components/backoffice.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ac-backoffice',
   template: `
-    <p>
-      backoffice works!
-    </p>
+    <!--navigation-->
+    <div class="navigation" style="display: flex; justify-content: center; align-content: center">
+      <button (click)="page = 'hero'">HERO</button>
+      <button (click)="page = 'news'">NEWS</button>
+      <button (click)="page = 'products'">PRODUCTS</button>
+    </div>
+
+    <!--switch case statement-->
+    <ng-container [ngSwitch]="page">
+      <ac-backoffice-hero *ngSwitchCase="'hero'"></ac-backoffice-hero>
+      <ac-backoffice-news *ngSwitchCase="'news'"></ac-backoffice-news>
+      <ac-backoffice-products *ngSwitchCase="'products'"></ac-backoffice-products>
+      <h2 *ngSwitchDefault>I will never be printed.</h2>
+    </ng-container>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class BackofficeComponent implements OnInit {
 
-  constructor() { }
+  page: 'hero' | 'news' | 'products' = 'hero';
 
+  /**
+   * The constructor method.
+   */
+  constructor() {
+  }
+
+  /**
+   * The ngOnInit method.
+   */
   ngOnInit(): void {
   }
 
