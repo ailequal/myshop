ceb0efa662ec8605b7acb976c7070ed5c2afe104
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-14 10:22:23 +0200

    Moved code into new components location.

--- src/app/features/cart/components/cart.component.ts
+++ src/app/features/cart/components/cart.component.ts
@@ -1,20 +1,198 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgModel} from "@angular/forms";
 
 @Component({
   selector: 'ac-cart',
   template: `
-    <p>
-      cart works!
-    </p>
+    <div class="cart container bg-light my-3 py-3 round-border">
+
+      <div class="py-5 text-center">
+        <i class="fab fa-shopify fa-4x"></i>
+        <h2>Checkout form</h2>
+        <p class="lead">Fill all fields and order now. Free shipping</p>
+      </div>
+
+      <!--MESSAGE PLACEHOLDER-->
+      <div class="row g-5">
+        <div class="col-md-5 col-lg-4 order-md-last">
+          <!--CART SUMMARY PLACEHOLDER-->
+          Cart Summary
+        </div>
+
+        <!--cart form-->
+        <form #f="ngForm" (submit)="submitHandler(f.value)" class="col-md-7 col-lg-8">
+          <h4 class="mb-3">Billing address</h4>
+
+          <!--all the form inputs-->
+          <div class="row g-3">
+            <div class="col-sm-6">
+              <label for="firstName" class="form-label">First name</label>
+              <input id="firstName" type="text" class="form-control" required ngModel name="firstName"
+                     #firstNameRef="ngModel"
+                     [ngClass]="checkField(firstNameRef)"
+              >
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+
+            <div class="col-sm-6">
+              <label for="lastName" class="form-label">Last name</label>
+              <input id="lastName" type="text" class="form-control" required ngModel name="lastName"
+                     #lastNameRef="ngModel"
+                     [ngClass]="checkField(lastNameRef)"
+              >
+              <div class="invalid-feedback">
+                Valid last name is required.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
+              <input id="email" type="email" class="form-control" placeholder="you@example.com" ngModel name="email"
+                     required email
+                     #emailRef="ngModel" [ngClass]="checkField(emailRef)">
+              <div class="invalid-feedback">
+                Please enter a valid email address for shipping updates.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="address" class="form-label">Address</label>
+              <input id="address" type="text" class="form-control" placeholder="1234 Main St" required ngModel
+                     name="address"
+                     #addressRef="ngModel" [ngClass]="checkField(addressRef)">
+              <div class="invalid-feedback">
+                Please enter your shipping address.
+              </div>
+            </div>
+
+            <div class="col-md-5">
+              <label for="country" class="form-label">Country</label>
+              <select id="country" class="form-select" required ngModel name="country" #countryRef="ngModel"
+                      [ngClass]="checkField(countryRef)">
+                <option disabled [value]="null">Choose...</option>
+                <option value="it">Italy</option>
+                <option value="us">United States</option>
+              </select>
+              <div class="invalid-feedback">
+                Please select a valid country.
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <label for="zip" class="form-label">Zip</label>
+              <input id="zip" type="text" class="form-control" required ngModel name="zip" minlength="4" maxlength="5"
+                     #zipRef="ngModel" [ngClass]="checkField(zipRef)">
+              <div class="invalid-feedback">
+                Zip code required (4 or 5 chars)
+              </div>
+            </div>
+          </div>
+
+          <hr class="my-4">
+
+          <!--payment options-->
+          <h4 class="mb-3">Payment</h4>
+          <div class="my-3">
+            <div class="form-check">
+              <input id="credit" type="radio" ngModel name="paymentMethod" value="creditCard" required
+                     class="form-check-input">
+              <label class="form-check-label" for="credit">Credit card</label>
+            </div>
+            <div class="form-check">
+              <input id="debit" type="radio" ngModel name="paymentMethod" value="bankTransfer" required
+                     class="form-check-input">
+              <label class="form-check-label" for="debit">Bank Transfer</label>
+            </div>
+          </div>
+
+          <!--credit card-->
+          <div class="row gy-3" *ngIf="f.value.paymentMethod === 'creditCard'">
+            <div class="col-md-6">
+              <label for="cc-name" class="form-label">Name on card</label>
+              <input id="cc-name" type="text" class="form-control" required ngModel name="cardName"
+                     #cardNameRef="ngModel"
+                     [ngClass]="checkField(cardNameRef)">
+              <small class="text-muted">Full name as displayed on card</small>
+              <div class="invalid-feedback">
+                Name on card is required
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <label for="cc-number" class="form-label">Credit card number</label>
+              <input id="cc-number" type="text" class="form-control" required minlength="16" maxlength="16" ngModel
+                     name="cardNumber" pattern="^[0-9]*$"
+                     #cardNumberRef="ngModel" [ngClass]="checkField(cardNumberRef)">
+              <div class="invalid-feedback">
+                Credit card number is required (16 numbers)
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <label for="cc-expiration" class="form-label">Expiration</label>
+              <input id="cc-expiration" type="text" class="form-control" placeholder="12/22"
+                     required pattern="^(0[1-9]|1[0-2])\\/?([0-9]{4}|[0-9]{2})$"
+                     ngModel name="cardExpiration" #cardExpirationRef="ngModel"
+                     [ngClass]="checkField(cardExpirationRef)">
+              <div class="invalid-feedback">
+                Expiration date required {{cardExpirationRef.errors | json}}
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <label for="cc-cvv" class="form-label">CVV</label>
+              <input id="cc-cvv" type="text" class="form-control" required minlength="3" maxlength="3" ngModel
+                     name="cardCVV" pattern="^[0-9]*$"
+                     #cardCVVRef="ngModel" [ngClass]="checkField(cardCVVRef)">
+              <div class="invalid-feedback">
+                Security code required
+              </div>
+            </div>
+          </div>
+
+          <hr class="my-4">
+
+          <button class="w-100 btn btn-primary btn-lg" type="submit" [disabled]="f.invalid">Confirm Order</button>
+        </form>
+      </div>
+    </div>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * The constructor method.
+   */
+  constructor() {
+  }
 
+  /**
+   * The ngOnInit method.
+   */
   ngOnInit(): void {
+  }
+
+  /**
+   * Check the passed field and return
+   *
+   * @param input
+   *
+   * @return Object
+   */
+  checkField(input: NgModel): Object {
+    return {'is-invalid': input.invalid, 'is-valid': input.valid}
+  }
+
+  /**
+   * Handles the cart form submission.
+   *
+   * @param formData
+   */
+  submitHandler(formData: any) {
+    console.log(formData)
   }
 
 }
--- src/app/features/shop/components/shop.component.ts
+++ src/app/features/shop/components/shop.component.ts
@@ -1,20 +1,253 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Hero} from "../../../shared/model/hero";
+import {News} from "../../../shared/model/news";
+import {Product} from "../../../shared/model/product";
 
 @Component({
   selector: 'ac-shop',
   template: `
-    <p>
-      shop works!
-    </p>
+    <!--main container-->
+    <div class="shop container">
+
+      <!--hero-->
+      <div class="card round-border overflow-hidden mt-3" style="height: 300px">
+        <img
+          class="card-img round-border"
+          style="object-fit: cover"
+          [src]="hero?.image" alt="Hero"
+        >
+        <div class="card-img-overlay mt-5">
+          <h1 class="card-title">{{hero?.title}}</h1>
+          <h4 class="card-text" [innerHTML]="hero?.description"></h4>
+        </div>
+      </div>
+
+      <!--cards container-->
+      <div class="row row-cols-1 row-cols-lg-2  row-cols-xl-3 mt-5">
+        <!--cards loop-->
+        <div class="col mb-3" *ngFor="let product of products">
+          <!--card-->
+          <div class="card h-100 round-border shadow-lg">
+            <!--image-->
+            <div class="position-relative">
+              <img
+                class="card-img-top p-3 round-border "
+                [src]="product.image" [alt]="product.label"
+                role="button"
+              >
+              <div class="position-absolute top-50 start-50 w-75 translate-middle pe-none">
+                <div class="row  row-cols-3 g-2 rounded p-2 text-white" style="background: rgba(0,0,0,0.5)">
+                  <div class="col text-center">
+                    <i class="fas fa-memory"></i>
+                    {{product.memory / 1000 }}
+                  </div>
+
+                  <div class="col text-center">
+                    <i class="fas fa-sim-card"></i>
+                    {{product.storage / 1000}}
+                  </div>
+                  <div class="col text-center">
+                    <i class="fas fa-mobile-alt"></i>
+                    {{product.display}}''
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!--title and description-->
+            <div class="card-body text-center">
+              <h5 class="card-title">
+                {{product.label}}
+              </h5>
+              <p class="card-text text-secondary">{{product.description}}</p>
+            </div>
+
+            <!--colors-->
+            <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center">
+              <div
+                class="mx-1 mb-3 round-border shadow-sm"
+                style="width: 30px; height: 30px; border: 1px solid #fff"
+                role="button"
+                *ngFor="let color of product.colors"
+                (click)="selectedColor = color"
+                [style.backgroundColor]="color"
+                [style.border-width.px]="selectedColor === color ? 5 : 1"
+                [style.border-color]="selectedColor === color ? 'orange' : '#ccc'"
+              ></div>
+            </div>
+
+            <!--footer-->
+            <div class="card-footer d-flex justify-content-between align-items-center">
+              <div class="h5 text-secondary">{{product.price | currency: '€'}} + vat</div>
+
+              <div>
+                <span>select color</span>
+
+                <button
+                  class="btn icon-circle-sm px-1 ms-2 shadow-lg"
+                  style="color: #bbb"
+                  [style.background-color]="selectedColor  || '#ccc'"
+                  [disabled]="!selectedColor"
+                  (click)="addToCartHandler({ product: product, color: selectedColor })"
+                >
+                  <i class="fas fa-cart-plus text-white"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--news container-->
+      <div class="row 3 mt-5">
+        <!--news loop-->
+        <div class="col-sm-6" *ngFor="let n of news">
+          <!--news-->
+          <div class="card round-border ">
+            <div class="card-body">
+              <h5 class="card-title">{{n.title}}</h5>
+              <p class="card-text">{{n.description}}</p>
+              <a [href]="n.url" target="_blank" class="btn btn-dark round-border">
+                <i class="fas fa-external-link-alt"></i>
+                Visit
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--newsletter-->
+      <div class="bg-dark text-white mt-5">
+        <div class="container py-5 text-center">
+          <i class="fab fa-shopify fa-4x"></i>
+
+          <h1 class="">Subscribe the newsletter</h1>
+
+          <div>Subscribe our newsletter to get notified about news and updates</div>
+
+          <div class="d-flex justify-content-center mt-2">
+            <form class="row g-3" #f="ngForm" (ngSubmit)="send(f.value.email)">
+              <div class="col-auto">
+                <input
+                  required
+                  type="email"
+                  placeholder="Your email address"
+                  name="email"
+                  pattern="^[A-Za-z0-9](([_\\.\\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\\.\\-]?[a-zA-Z0-9]+)*)\\.([A-Za-z]{2,})$"
+                  class="form-control form-control-lg"
+                  #emailRef="ngModel"
+                  [ngModel]="subscribed"
+                  [readOnly]="subscribed"
+                  [ngClass]="{'is-invalid': emailRef.invalid && f.dirty, 'is-valid': emailRef.valid}"
+                >
+              </div>
+              <div class="col-auto">
+                <button
+                  type="submit" class="btn btn-lg btn-primary mb-3"
+                  [disabled]="subscribed || f.invalid"
+                >Subscribe
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
   `,
-  styles: [
-  ]
+  styles: [`
+    .card img:hover {
+      -webkit-box-shadow: 0 0 100px 0 rgb(186, 186, 186);
+      -moz-box-shadow: 0 0 100px 0 rgb(186, 186, 186);
+      box-shadow: 0 0 100px 0 rgb(186, 186, 186);
+    }
+  `]
 })
 export class ShopComponent implements OnInit {
 
-  constructor() { }
+  hero: Hero | null = null;
 
+  products: Product[] = [];
+
+  news: News[] = [];
+
+  subscribed: string | null = null;
+
+  // TODO: The selected color is the same for all the available products.
+  //  This bug will be fixed later on. Ignore it for now.
+  selectedColor: string | null = null;
+
+  /**
+   * The constructor method.
+   * The HttpClient instance is injected.
+   */
+  constructor(private http: HttpClient) {
+  }
+
+  /**
+   * The ngOnInit method.
+   */
   ngOnInit(): void {
+    this.http.get<Hero>('http://localhost:3000/hero')
+      .subscribe({
+        next: (v) => this.hero = v,
+        error: (e) => console.log(e),
+        complete: () => console.log('Completed http.get<Hero>().')
+      });
+
+    this.http.get<Product[]>('http://localhost:3000/products')
+      .subscribe({
+        next: (v) => this.products = v,
+        error: (e) => console.log(e),
+        complete: () => console.log('Completed http.get<Product[]>().')
+      });
+
+    this.http.get<News[]>('http://localhost:3000/news')
+      .subscribe({
+        next: (v) => this.news = v,
+        error: (e) => console.log(e),
+        complete: () => console.log('Completed http.get<News[]>().')
+      });
+
+    this.subscribed = localStorage.getItem('subscribed')
+  }
+
+  /**
+   * Add the passed product and its color to the cart.
+   *
+   * @param params
+   */
+  addToCartHandler(params: { product: Product; color: string | null }): void {
+    console.log(params.product, params.color)
+  }
+
+  /**
+   * Handles the newsletter form submission.
+   *
+   * @param email
+   */
+  send(email: string): void {
+    // We simulate the newsletter subscription through a simple GET.
+    // The db.json has a corresponding resource, that we will check.
+    this.http.get<{ response: string }>(
+      'http://localhost:3000/newsletter',
+      {
+        params: {
+          email: email
+        }
+      })
+      .subscribe({
+        next: (v) => {
+          if (v.response === 'ok') {
+            this.subscribed = email
+            localStorage.setItem('subscribed', email)
+            alert('Subscribed successfully!!')
+          }
+        },
+        error: (e) => console.log(e),
+        complete: () => console.log('Completed http.get<{ response: string }>().')
+      });
   }
 
 }
