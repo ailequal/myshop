ec13429c14132db6a2959ab98d159034fc6cb904
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-20 11:44:52 +0200

    Prettify the ProductComponent.

--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -6,7 +6,19 @@ import {Product} from '../../shared/model/product';
 @Component({
   selector: 'ac-product',
   template: `
-    <pre>{{product | json}}</pre>
+    <div class="px-4 pt-5 my-5 text-center border-bottom">
+      <h1 class="display-4 fw-bold">{{product?.label}}</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">{{product?.description}}</p>
+
+        <!--TODO: Select Color Button placeholder-->
+      </div>
+      <div>
+        <img [src]="product?.image" class="shadow-lg p-4" width="100%" style="max-width: 50vh; margin: 0 auto" alt="#">
+      </div>
+      <!--TODO: The shop route should not be hardcoded like this...-->
+      <button class="btn btn-link" routerLink="/shop">Back To Shop</button>
+    </div>
   `
 })
 export class ProductComponent implements OnInit {
@@ -27,7 +39,7 @@ export class ProductComponent implements OnInit {
    */
   ngOnInit(): void {
     // Get the product id from the current url.
-    const productId: string = this.activatedRoute.snapshot.params['id'];
+    const productId: number = parseInt(this.activatedRoute.snapshot.params['id']);
 
     // Get the whole product information.
     this.http.get<Product>(`http://localhost:3000/products/${productId}`).subscribe({
