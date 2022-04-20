a95f37d3d1f1f422f49696bae3201e4489a0fb7e
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-20 11:38:41 +0200

    Getting single product information.

--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -1,26 +1,40 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Product} from '../../shared/model/product';
 
 @Component({
   selector: 'ac-product',
   template: `
-    <p>
-      product works!
-    </p>
-  `,
-  styles: []
+    <pre>{{product | json}}</pre>
+  `
 })
 export class ProductComponent implements OnInit {
+
+  product: Product | null = null;
 
   /**
    * The constructor method.
    */
-  constructor() {
+  constructor(
+    private http: HttpClient,
+    private activatedRoute: ActivatedRoute
+  ) {
   }
 
   /**
    * The ngOnInit method.
    */
   ngOnInit(): void {
+    // Get the product id from the current url.
+    const productId: string = this.activatedRoute.snapshot.params['id'];
+
+    // Get the whole product information.
+    this.http.get<Product>(`http://localhost:3000/products/${productId}`).subscribe({
+      next: (v) => this.product = v,
+      error: (e) => console.log(e),
+      complete: () => console.log('Completed http.get<Product>().')
+    });
   }
 
 }
