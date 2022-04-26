391f3bda3d1a2f72b9bac9d6efa20587caa45cfc
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-26 11:04:43 +0200

    Refactoring.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProductComponent} from "./features/product/product.component";
+import {CartComponent} from "./features/cart/cart.component";
+import {PageNotFoundComponent} from "./features/page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'shop',
+    loadChildren: () => import('./features/shop/shop.module').then(m => m.ShopModule),
+    pathMatch: 'full'
+  },
+  {path: 'product/:id', component: ProductComponent, pathMatch: 'full'},
+  {path: 'cart', component: CartComponent, pathMatch: 'full'},
+  {
+    path: 'backoffice',
+    loadChildren: () => import('./features/backoffice/backoffice.module').then(m => m.BackofficeModule),
+    pathMatch: 'full'
+  },
+  {path: 'page-not-found', component: PageNotFoundComponent, pathMatch: 'full'},
+  {path: '', redirectTo: 'shop', pathMatch: 'full'},
+  {path: '**', redirectTo: 'page-not-found', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,46 +7,22 @@ import {HttpClientModule} from "@angular/common/http";
 import {SharedModule} from './shared/shared.module';
 
 import {AppComponent} from './app.component';
-import {ShopComponent} from './features/shop/shop.component';
 import {CartComponent} from './features/cart/cart.component';
-import {ShopHeroComponent} from './features/shop/components/shop-hero.component';
-import {ShopItemCardComponent} from './features/shop/components/shop-item-card.component';
-import {ShopItemNewsComponent} from './features/shop/components/shop-item-news.component';
-import {ShopItemNewsletterComponent} from './features/shop/components/shop-item-newsletter.component';
 import {PageNotFoundComponent} from './features/page-not-found/page-not-found.component';
 import {ProductComponent} from './features/product/product.component';
 import {BackofficeModule} from './features/backoffice/backoffice.module';
 
-const routes: Routes = [
-  {path: 'shop', component: ShopComponent, pathMatch: 'full'},
-  {path: 'product/:id', component: ProductComponent, pathMatch: 'full'},
-  {path: 'cart', component: CartComponent, pathMatch: 'full'},
-  {
-    path: 'backoffice',
-    loadChildren: () => import('./features/backoffice/backoffice.module').then(m => m.BackofficeModule),
-    pathMatch: 'full'
-  },
-  {path: 'page-not-found', component: PageNotFoundComponent, pathMatch: 'full'},
-  {path: '', redirectTo: 'shop', pathMatch: 'full'},
-  {path: '**', redirectTo: 'page-not-found', pathMatch: 'full'},
-];
-
 @NgModule({
   declarations: [
     AppComponent,
-    ShopComponent,
     CartComponent,
-    ShopHeroComponent,
-    ShopItemCardComponent,
-    ShopItemNewsComponent,
-    ShopItemNewsletterComponent,
     PageNotFoundComponent,
     ProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes),
+    RouterModule,
     FormsModule,
     HttpClientModule,
     SharedModule,
