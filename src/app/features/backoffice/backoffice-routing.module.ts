1e13667db25ee86b697f47f82c01068abe9245ca
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-26 13:43:35 +0200

    Good routing.

--- src/app/features/backoffice/backoffice-routing.module.ts
+++ src/app/features/backoffice/backoffice-routing.module.ts
@@ -4,21 +4,27 @@ import {BackofficeComponent} from "./backoffice.component";
 
 const routes: Routes = [
   {
-    path: 'hero',
-    loadChildren: () => import('./components/hero/hero.module').then(m => m.HeroModule),
-    pathMatch: 'full'
-  },
-  {
-    path: 'news',
-    loadChildren: () => import('./components/news/news.module').then(m => m.NewsModule),
-    pathMatch: 'full'
-  },
-  {
-    path: 'products',
-    loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule),
-    pathMatch: 'full'
-  },
-  {path: '', component: BackofficeComponent, pathMatch: 'full'}
+    path: '',
+    component: BackofficeComponent,
+    children: [
+      {
+        path: 'hero',
+        loadChildren: () => import('./components/hero/hero.module').then(m => m.HeroModule),
+        pathMatch: 'full'
+      },
+      {
+        path: 'news',
+        loadChildren: () => import('./components/news/news.module').then(m => m.NewsModule),
+        pathMatch: 'full'
+      },
+      {
+        path: 'products',
+        loadChildren: () => import('./components/products/products.module').then(m => m.ProductsModule),
+        pathMatch: 'full'
+      }
+    ],
+    // pathMatch: 'full' // It's disabled, since there are sub routes!!
+  }
 ];
 
 @NgModule({
