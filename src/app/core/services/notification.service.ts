c639eed9e948b64523ef69c9773ac99735249443
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-28 10:36:15 +0200

    Added basic notification service methods.

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -5,10 +5,29 @@ import {Injectable} from '@angular/core';
 })
 export class NotificationService {
 
+  message: string | null = null;
+
   /**
    * The constructor method.
    */
   constructor() {
+  }
+
+  /**
+   * Show the message.
+   *
+   * @param message
+   */
+  show(message: string): void {
+    this.message = message;
+    setTimeout(this.hide, 3000);
+  }
+
+  /**
+   * Hide the message.
+   */
+  hide(): void {
+    this.message = null;
   }
 
 }
