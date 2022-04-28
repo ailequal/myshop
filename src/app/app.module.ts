d8feb3e417f05811943bcaee3f95dbde901d11b3
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-28 21:21:04 +0200

    Application locale set to it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import {NgModule} from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {registerLocaleData} from "@angular/common";
+import it from "@angular/common/locales/it";
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from "./core/core.module";
 
 import {AppComponent} from './app.component';
+
+registerLocaleData(it)
 
 @NgModule({
   declarations: [
@@ -14,7 +18,7 @@ import {AppComponent} from './app.component';
     AppRoutingModule,
     CoreModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'it'}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
