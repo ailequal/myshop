571c3686e9be9f30001475b9975f4632003e3079
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-28 11:18:23 +0200

    Basic methods for cart service.

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,14 +1,47 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NotificationService} from './notification.service';
+import {CartItem} from '../model/cart-item';
+import {Product} from '../../shared/model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  items: CartItem[] = [];
+
   /**
    * The constructor method.
    */
-  constructor() {
+  constructor(
+    private http: HttpClient,
+    private notificationService: NotificationService
+  ) {
+  }
+
+  addItem(itemToAdd: Product, selectedColor: string | null): void {
+    console.log(itemToAdd, selectedColor)
+  }
+
+  removeItem(itemToRemove: CartItem): void {
+    console.log(itemToRemove)
+  }
+
+  incrementQuantity(itemToUpdate: CartItem): void {
+    console.log(itemToUpdate)
+  }
+
+  decrementQuantity(itemToUpdate: CartItem): void {
+    console.log(itemToUpdate)
+  }
+
+  orderNow(formData: any): void {
+    console.log(formData, this.items)
+  }
+
+  getTotalCartAmount(): number {
+    return 0;
   }
 
 }
