1c086f00dbe99ed59485a8fff701f2e88b6bad71
ailequal <37016865+ailequal@users.noreply.github.com>
2022-04-28 10:42:30 +0200

    Added notification component template.

--- src/app/core/components/notification.component.ts
+++ src/app/core/components/notification.component.ts
@@ -1,11 +1,27 @@
 import {Component, OnInit} from '@angular/core';
+import {NotificationService} from "../services/notification.service";
 
 @Component({
   selector: 'ac-notification',
   template: `
-    <p>
-      notification works!
-    </p>
+    <div
+      *ngIf="notificationService.message"
+      class="position-fixed start-0 p-3 ms-5" style="z-index: 11; top: 50px"
+    >
+
+      <div class="toast show align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+          <h4 class="toast-body p-3">
+            {{notificationService.message}}
+          </h4>
+
+          <button
+            (click)="notificationService.hide()"
+            type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"
+          ></button>
+        </div>
+      </div>
+    </div>
   `,
   styles: []
 })
@@ -14,7 +30,7 @@ export class NotificationComponent implements OnInit {
   /**
    * The constructor method.
    */
-  constructor() {
+  constructor(public notificationService: NotificationService) {
   }
 
   /**
